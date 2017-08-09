b50712cf7939c1fb330e1694f4b8f37b16053604
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:50:09 -0500

    Hollens solutions

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+var pinkButton = $("#pink")
+console.log(pinkButton)
+pinkButton.click(function(){
+  pinkButton.css("border","20px solid pink");
+});
+
+var blueButton = $("#blue");
+blueButton.click(function(){
+$("#blueSquare").
+css("background_color","blue");
+
+})
 
 })
