525d7cbcceb928f5e682f90c0645045f5ab0b1ea
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:04:17 -0500

    Atom's branch

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
-
-})
+  var pinkPink = $('#pink');
+  console.log(pinkPink);
+  pinkPink.click(function() {
+    pinkPink.css("border", "20px solid pink");
+});
+  var bloo = $('#blue');
+  bloo.click(function() {
+    $("#div2").css("background-color", "#4d75b7");
+});
+  var fadingAway = $('#fade');
+  fadingAway.click(function() {
+    fadingAway.fadeOut(10000);
+});
+});
