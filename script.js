d18a86a17e1b0ed1b8397c2f3dc331c08892f016
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:06:12 -0500

    davis solutions

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-// your javascript and jQuery code goes below
+
+var pinkButton = $("#pink");
+console.log(pinkButton)
+
+pinkButton.click(function(){
+  //console.log("hello")
+  pinkButton.css("border", "20px solid pink");
+});
+
+var blueButton = $("#blue");
+
+blueButton.click(function(){
+
+blueButton.css("border", "20px solid blue");
+
+});
 
 })
