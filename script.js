bb5833f85f8ee83a792b9f200258abe8633215ac
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:49:47 -0500

    Max's code

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
-// your javascript and jQuery code goes below
+var pinkButton = $('#pink');
+
+pinkButton.click(function(){
+  pinkButton.css("border", "20px solid pink" );
+
+});
+
+var blueButton = $('#blue');
+
+blueButton.click(function(){
+  $("#backgroundBlue").css("background-color", "skyblue" );
+
+});
+
+var fadeAway = $('#fadeOut')
+fadeAway.fadeOut(5000)
+
+});
+
+var lottery = $('#lottery')
+lottery.click(prompt("YOU WON THE POWERBALL!!!!!"))
 
 })
