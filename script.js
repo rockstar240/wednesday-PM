c4b8c217162860c085cbdc28740536d8d462f142
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:03:19 -0500

    zach's code

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+var pinkButton = $('#pink');
+console.log(pinkButton);
+pinkButton.click(function(){
+  pinkButton.css("border", "20px solid pink");
+});
 
-})
+var blueButton = $('#blue');
+console.log(blueButton);
+blueButton.click(function(){
+  $("#blueBack").css("background-color", "#1379c1");
+});
+
+var fadeButton = $('#fade');
+
+});
