39ffd1559610779c40f1fbdfd6249073899380bd
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:49:19 -0500

    sydneys submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-// your javascript and jQuery code goes below
-
+$(document)
+.ready(function() {
+var pinkButton = $('#pink')
+console.log(pinkButton)
+pinkButton.click(function(){
+pinkButton.css("border","10px solid pink")
+});
+var blueButton = $('#blue')
+console.log(blueButton)
+blueButton.click(function(){
+blueButton.css("background-color","#5bc5ff")
 })
+
+var fadeButton = $('#fade')
+console.log(fadeButton)
+fadeButton.click(function(){
+fadeButton.fadeOut("8000")
+})
+
+});
